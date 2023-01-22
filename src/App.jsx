63a3ed034ed701b44b94bc8bbcc337630860b4cd
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-22 19:07:28 +0530

    social media section progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 import { useGlobalContext } from './store/context';
 import Navbar from './sections/Navbar';
+import SocialMediaSection from './sections/SocialMediaSection';
 
 function App() {
   const { theme } = useGlobalContext();
   return (
     <main>
       <Navbar />
+      <SocialMediaSection />
     </main>
   );
 }
