1028ea9b27bc8560a8ff64037224fc87f5544aa7
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-22 19:06:30 +0530

    social media section created

---
+++ src/sections/SocialMediaSection.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import SocialMediaCard from '../components/Card/SocialMediaCard';
+import Facebook from '../assets/icon-facebook.svg';
+import Twitter from '../assets/icon-twitter.svg';
+import Instagram from '../assets/icon-instagram.svg';
+import YouTube from '../assets/icon-youtube.svg';
+
+const SocialMediaSection = () => {
+  return (
+    <section className="relative">
+      <div className="mx-auto px-6 flex flex-col gap-6 absolute -top-11 left-1/2 transform -translate-x-1/2">
+        <SocialMediaCard
+          iconImage={Facebook}
+          name="@nathanf"
+          count="1987"
+          countType="followers"
+          isIncreasing={true}
+          followersCount="12"
+          styles="border-blue-800"
+        />
+
+        <SocialMediaCard
+          iconImage={Twitter}
+          name="@nathanf"
+          count="1044"
+          countType="followers"
+          isIncreasing={true}
+          followersCount="99"
+          styles="border-blue-700"
+        />
+
+        <SocialMediaCard
+          iconImage={Twitter}
+          name="@realnathanf"
+          count="11K"
+          countType="followers"
+          isIncreasing={true}
+          followersCount="1099"
+          styles="border-[#e95950]"
+        />
+
+        <SocialMediaCard
+          iconImage={YouTube}
+          name="Nathan F."
+          count="8239"
+          countType="SUBSCRIBERS"
+          isIncreasing={false}
+          followersCount="144"
+          styles="border-red-900"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SocialMediaSection;
