d4f2430228fc7f301f71a6733ac1747ac8c24180
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-23 17:03:18 +0530

    min height set to screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import classNames from 'classnames';
 import { useGlobalContext } from './store/context';
 import Navbar from './sections/Navbar';
 import SocialMediaSection from './sections/SocialMediaSection';
+import OverviewSection from './sections/OverviewSection';
 
 function App() {
   const { theme } = useGlobalContext();
   return (
-    <main className={classNames(theme ? 'bg-white' : 'bg-dark-800')}>
+    <main
+      className={classNames(theme ? 'bg-white' : 'bg-dark-800', 'min-h-screen')}
+    >
       <Navbar />
       <SocialMediaSection />
+      <OverviewSection />
     </main>
   );
 }
