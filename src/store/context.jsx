af465bc548efd47b92270d08a6299baf2cfd87b5
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-23 17:15:07 +0530

    theme status set to local storage

--- src/store/context.jsx
+++ src/store/context.jsx
@@ -1,12 +1,16 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(localStorage.getItem('theme' || false));
   const changeTheme = () => {
     setTheme(!theme);
   };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
     <AppContext.Provider value={{ theme, changeTheme }}>
       {children}
